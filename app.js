cdf52c088cfad9bbb7069d22d8934b4048bd1e4b
Kim-Link <wildrays@gmail.com>
2021-06-24 16:03:56 +0900

    first server file

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const indexRouter = require('./routes/index');
+const fs = require('fs');
+const https = require('https');
+
+dotenv.config();
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+//서버 설정
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+    cors({
+      origin: ["https://localhost:3000"],
+      credentials: true,
+      methods: ["GET", "POST", "OPTIONS"],
+    })
+  );
+
+  
+
+
+  module.exports = server;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    board: require('./controllers/board'), 
+    jazzbar: require('./controllers/jazzbar'),
+    login: require('./controllers/login'),
+    menu: require('./controllers/menu'),
+    oauth: require('./controllers/OAuth'),
+    reservation: require('./controllers/reservation'),
+    review: require('./controllers/review'),
+    show: require('./controllers/show'),
+    user: require('./controllers/user'),
+    
+  };
